81dabed713a63a7dcb6d7bce3def6fa26a0848e1
johmstep33 <johmstep33@gmail.com>
2015-12-14 18:24:31 -0600

    First example commit

---
+++ array_props.js
@@ -0,0 +1,21 @@
+var bucketList = [];
+
+bucketList [0] = "Ride a hotair balloon"
+bucketList [1] = "Backpack all over Ireland"
+bucketList [2] = "Live in another country"
+bucketList [3] = "Fall in Love"
+
+bucketList.push('Move to New York City');
+bucketList.push('Become an Art Director');
+
+bucketList.unshift('Own a Pug');
+bucketList.unshift('Visit every continent');
+
+bucketList.pop();
+var dyingWish = ['Become an Art Director'];
+
+bucketList.shift();
+var doItNow = ["Visit every continent"];
+bucketList
+dyingWish
+doItNow
